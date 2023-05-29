ebec61ae6f4f4d5b5ecfc43b66b8b89116026ca1
Erykdev06 <lamnguyen062003@gmail.com>
2023-05-29 23:59:03 +0700

    update file

--- App.js
+++ App.js
@@ -1,3 +1,3 @@
-import App from './src';
+import App from '@/App';
 
 export default App;
---
+++ src/components/Demo.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Demo = () => {
+  return (
+    <View>
+      <Text>Text</Text>
+    </View>
+  );
+};
+
+export default Demo;
+
+const styles = StyleSheet.create({});
