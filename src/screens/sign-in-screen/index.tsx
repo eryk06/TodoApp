8e9e602a6ee180dca445ee2dde1c67abe140c922
Erykdev06 <lamnguyen062003@gmail.com>
2023-06-03 23:36:01 +0700

    add file signin

--- src/screens/sign-in-screen/index.tsx
+++ src/screens/sign-in-screen/index.tsx
@@ -1,24 +1,40 @@
+import Button from '@/components/shared/button';
+import Input from '@/components/shared/input';
 import SafeAreaWrapper from '@/components/shared/safe-area-wrapper';
 import { AuthScreenNavigationType } from '@/navigation/types';
 import { Box, Text } from '@/utils/theme';
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { Button } from 'react-native';
+import { Pressable } from 'react-native';
 
-const SignIn = () => {
+const SignInScreen = () => {
   const navigation = useNavigation<AuthScreenNavigationType<'SignIn'>>();
-  const navigateToSignUpScreen = () => {
+  const navigateToSignInScreen = () => {
     navigation.navigate('SignUp');
   };
 
   return (
     <SafeAreaWrapper>
-      <Box>
-        <Text>Sign In Screen</Text>
-        <Button title="Navigate to sign up" onPress={navigateToSignUpScreen} />
+      <Box flex={1} px="5.5" justifyContent="center">
+        <Text variant="textXl" fontWeight="700">
+          Welcome Back
+        </Text>
+
+        <Box mb="6" />
+        <Input label="Email" />
+        <Box mb="6" />
+        <Input label="Password" />
+        <Box mt="5.5" />
+        <Pressable onPress={navigateToSignInScreen}>
+          <Text color="primary" textAlign="right">
+            Register?
+          </Text>
+        </Pressable>
+        <Box mb="5.5" />
+        <Button label="Login" onPress={navigateToSignInScreen} uppercase />
       </Box>
     </SafeAreaWrapper>
   );
 };
 
-export default SignIn;
+export default SignInScreen;
