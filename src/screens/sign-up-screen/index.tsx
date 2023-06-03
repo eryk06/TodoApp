8ca4718c4d156205b756cc2a0005f80713f10ecc
Erykdev06 <lamnguyen062003@gmail.com>
2023-06-03 23:23:10 +0700

    add file sing up

--- src/screens/sign-up-screen/index.tsx
+++ src/screens/sign-up-screen/index.tsx
@@ -1,9 +1,10 @@
+import Button from '@/components/shared/button';
+import Input from '@/components/shared/input';
 import SafeAreaWrapper from '@/components/shared/safe-area-wrapper';
 import { AuthScreenNavigationType } from '@/navigation/types';
 import { Box, Text } from '@/utils/theme';
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { Button } from 'react-native';
 
 const SignUpScreen = () => {
   const navigation = useNavigation<AuthScreenNavigationType<'SignUp'>>();
@@ -13,9 +14,21 @@ const SignUpScreen = () => {
 
   return (
     <SafeAreaWrapper>
-      <Box>
-        <Text>Sign Up Screen</Text>
-        <Button title="Navigate to sign in" onPress={navigateToSignInScreen} />
+      <Box flex={1} px="5.5" mt={'13'}>
+        <Text variant="textXl" fontWeight="700">
+          Welcome to Todo List!
+        </Text>
+        <Text variant="textXl" fontWeight="700" mb="6">
+          Your journey starts here
+        </Text>
+
+        <Input label="Name" />
+        <Box mb="6" />
+        <Input label="Email" />
+        <Box mb="6" />
+        <Input label="Password" />
+        <Box height={44} />
+        <Button label="Register" onPress={navigateToSignInScreen} uppercase />
       </Box>
     </SafeAreaWrapper>
   );
